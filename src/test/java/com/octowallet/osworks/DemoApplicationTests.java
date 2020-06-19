cff7a7b494f5e23d180e3b22bebfa5b06bf65097
George Matos <georgemattos@gmail.com>
2020-06-19 15:48:17 -0300

    criacao do projeto + ClientController + Cliente + conexao com o banco de dados

---
+++ src/test/java/com/octowallet/osworks/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octowallet.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
